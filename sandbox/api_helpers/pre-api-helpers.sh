9cc60a16137865d6caef2fa24c58f4ed2c58c9cf
Momcilo Dejanovic <momcilo.dejanovic.reyktech@sonymusic.com>
2023-07-27 00:55:29 +0200

    Test pre api helpers

--- sandbox/api_helpers/pre-api-helpers.sh
+++ sandbox/api_helpers/pre-api-helpers.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo "Executing Pre-API Helpers"
+
+ACCOUNT_ID=$(echo "${CODEBUILD_INITIATOR}" | grep -o '[0-9]\+')
+echo "Account ID: ${ACCOUNT_ID}"
+ACCOUNT_NAME=$(aws dynamodb get-item --table-name aft-request-metadata --key "{"id": {"S": "${ACCOUNT_ID}"}}" --query "Item.account_name.S")
+echo "Account Name: ${ACCOUNT_NAME}"
