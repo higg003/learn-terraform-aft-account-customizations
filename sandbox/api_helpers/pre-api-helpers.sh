e54ab09118a670d46105a99f7f2cd8feca90e935
Momcilo Dejanovic <momcilo.dejanovic.reyktech@sonymusic.com>
2023-07-27 12:19:18 +0200

    Test dynamodb table command

--- sandbox/api_helpers/pre-api-helpers.sh
+++ sandbox/api_helpers/pre-api-helpers.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 echo "Executing Pre-API Helpers"
-
+printenv
+echo
+CUSTOMIZATION=$(aws dynamodb get-item --table-name aft-request-metadata --key "{\"id\": {\"S\": \"$VENDED_ACCOUNT_ID\"}}" --attributes-to-get "account_customizations_name" | jq --raw-output ".Item.account_customizations_name.S")
+echo "Customization: ${CUSTOMIZATION}"
 TEST=$(aws dynamodb list-tables --region us-east-1)
 echo "Tables in DynamoDB: ${TEST}"
 ACCOUNT_ID=$(echo "${CODEBUILD_INITIATOR}" | grep -o '[0-9]\+')
