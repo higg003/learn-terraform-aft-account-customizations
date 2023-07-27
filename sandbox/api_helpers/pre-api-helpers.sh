23c620d0e1937b3a256f84781e9f0576a027cadc
Momcilo Dejanovic <momcilo.dejanovic.reyktech@sonymusic.com>
2023-07-27 13:28:46 +0200

    Cleanup pre api helper script

--- sandbox/api_helpers/pre-api-helpers.sh
+++ sandbox/api_helpers/pre-api-helpers.sh
@@ -4,7 +4,6 @@ echo "Executing Pre-API Helpers"
 
 export AWS_PROFILE=aft-management
 
-ACCOUNT_ID=$(echo "${CODEBUILD_INITIATOR}" | grep -o '[0-9]\+')
-echo "Account ID: ${ACCOUNT_ID}"
-ACCOUNT_NAME=$(aws dynamodb get-item --table-name aft-request-metadata --key "{\"id\": {\"S\": \"${ACCOUNT_ID}\"}}" --profile aft-management | jq -r '.Item.account_name.S')
+echo "Account ID: ${VENDED_ACCOUNT_ID}"
+ACCOUNT_NAME=$(aws dynamodb get-item --table-name aft-request-metadata --key "{\"id\": {\"S\": \"${VENDED_ACCOUNT_ID}\"}}" --profile aft-management | jq -r '.Item.account_name.S')
 echo "Account Name: ${ACCOUNT_NAME}"
