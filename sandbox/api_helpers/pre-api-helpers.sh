17fa616c0cc0ceea6a2eeb676711588f2e9e8588
Momcilo Dejanovic <momcilo.dejanovic.reyktech@sonymusic.com>
2023-07-27 14:14:21 +0200

    Injecting proper value to locals.tf

--- sandbox/api_helpers/pre-api-helpers.sh
+++ sandbox/api_helpers/pre-api-helpers.sh
@@ -5,9 +5,10 @@ echo "Executing Pre-API Helpers"
 export AWS_PROFILE=aft-management
 
 echo "Account ID: ${VENDED_ACCOUNT_ID}"
-ACCOUNT_NAME=$(aws dynamodb get-item --table-name aft-request-metadata --key "{\"id\": {\"S\": \"${VENDED_ACCOUNT_ID}\"}}" --profile aft-management | jq -r '.Item.account_name.S')
+ACCOUNT_NAME=$(aws dynamodb get-item --table-name aft-request-metadata --key "{\"id\": {\"S\": \"${VENDED_ACCOUNT_ID}\"}}" | jq -r '.Item.account_name.S')
 echo "Account Name: ${ACCOUNT_NAME}"
 echo "Adding Account Name to Terraform Locals"
+ACCOUNT_NAME=$(echo "$ACCOUNT_NAME" | rev | cut -d'-' -f2- | rev)
 sed -i "s|ACCOUNT_NAME|${ACCOUNT_NAME}|g" ${DEFAULT_PATH}/${CUSTOMIZATION}/terraform/locals.tf
 echo "Checking Locals file"
 cat ${DEFAULT_PATH}/${CUSTOMIZATION}/terraform/locals.tf
