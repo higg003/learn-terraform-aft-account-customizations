e7dc80532df29b6c434f235c38b614a4a3f5accb
Momcilo Dejanovic <momcilo.dejanovic.reyktech@sonymusic.com>
2023-07-27 11:53:23 +0200

    Test dynamodb table command

--- sandbox/api_helpers/pre-api-helpers.sh
+++ sandbox/api_helpers/pre-api-helpers.sh
@@ -2,7 +2,7 @@
 
 echo "Executing Pre-API Helpers"
 
-TEST=$(aws dynamodb list-tables)
+TEST=$(aws dynamodb list-tables --region us-east-1)
 echo "Tables in DynamoDB: ${TEST}"
 ACCOUNT_ID=$(echo "${CODEBUILD_INITIATOR}" | grep -o '[0-9]\+')
 echo "Account ID: ${ACCOUNT_ID}"
