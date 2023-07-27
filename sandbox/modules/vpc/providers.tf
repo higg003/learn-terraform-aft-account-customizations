c1321f70f88bbb761b2ed7ecc1166f5bab0ce269
Momcilo Dejanovic <momcilo.dejanovic.reyktech@sonymusic.com>
2023-07-27 23:09:53 +0200

    testing permissions

--- sandbox/modules/vpc/providers.tf
+++ sandbox/modules/vpc/providers.tf
@@ -1,13 +1,10 @@
 terraform {
   required_version = ">= 0.15.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
 }
 
 provider "aws" {
   region = var.aws_region
+  assume_role {
+    role_arn    = "arn:aws:iam::187438746603:role/AWSAFTExecution"
+  }
 }
