d2d1afb54043e884ffa6ae074ec8bb258b8b2460
Momcilo Dejanovic <momcilo.dejanovic.reyktech@sonymusic.com>
2023-07-27 13:05:03 +0200

    Test dynamodb table command

--- sandbox/api_helpers/pre-api-helpers.sh
+++ sandbox/api_helpers/pre-api-helpers.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 echo "Executing Pre-API Helpers"
-grep '\[.*\]' ~/.aws/credentials ~/.aws/config
-TEST=$(aws dynamodb list-tables --profile aft-management-admin)
+aws s3 ls --profile aft-management
 echo "Tables in DynamoDB: ${TEST}"
 ACCOUNT_ID=$(echo "${CODEBUILD_INITIATOR}" | grep -o '[0-9]\+')
 echo "Account ID: ${ACCOUNT_ID}"
-ACCOUNT_NAME=$(aws dynamodb get-item --table-name aft-request-metadata --key "{\"id\": {\"S\": \"${ACCOUNT_ID}\"}}")
+ACCOUNT_NAME=$(aws dynamodb get-item --table-name aft-request-metadata --key "{\"id\": {\"S\": \"${ACCOUNT_ID}\"}}" --profile aft-management | jq -r '.Item.account_name.S')
 echo "Account Name: ${ACCOUNT_NAME}"
