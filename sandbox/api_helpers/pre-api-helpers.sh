0acea4ce01111156c1201f5b061a7bd497583cb8
Momcilo Dejanovic <momcilo.dejanovic.reyktech@sonymusic.com>
2023-07-27 10:59:09 +0200

    Fix pre api helpers script

--- sandbox/api_helpers/pre-api-helpers.sh
+++ sandbox/api_helpers/pre-api-helpers.sh
@@ -4,5 +4,5 @@ echo "Executing Pre-API Helpers"
 
 ACCOUNT_ID=$(echo "${CODEBUILD_INITIATOR}" | grep -o '[0-9]\+')
 echo "Account ID: ${ACCOUNT_ID}"
-ACCOUNT_NAME=$(aws dynamodb get-item --table-name aft-request-metadata --key "{"id": {"S": "${ACCOUNT_ID}"}}" --query "Item.account_name.S")
+ACCOUNT_NAME=$(aws dynamodb get-item --table-name aft-request-metadata --key "{\"id\": {\"S\": \"${ACCOUNT_ID}\"}}" --query "Item.account_name.S")
 echo "Account Name: ${ACCOUNT_NAME}"
