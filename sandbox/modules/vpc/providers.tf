fd55956b4be518b9d7e7ac2a3e8d9719d19c1ed4
Momcilo Dejanovic <momcilo.dejanovic.reyktech@sonymusic.com>
2023-07-27 23:51:37 +0200

    Fix provider

--- sandbox/modules/vpc/providers.tf
+++ sandbox/modules/vpc/providers.tf
@@ -1,10 +1,10 @@
 terraform {
   required_version = ">= 0.15.0"
-}
 
-provider "aws" {
-  region = var.aws_region
-  assume_role {
-    role_arn    = "arn:aws:iam::187438746603:role/AWSAFTExecution"
-  }
+    required_providers {
+        aws = {
+        source  = "hashicorp/aws"
+        version = ">= 4.27.0"
+        }
+    }
 }
