7f9d158c4e44f455213408be7b8e8c17db1b63ef
Momcilo Dejanovic <momcilo.dejanovic.reyktech@sonymusic.com>
2023-07-27 13:59:16 +0200

    Testing substitution

--- sandbox/api_helpers/pre-api-helpers.sh
+++ sandbox/api_helpers/pre-api-helpers.sh
@@ -8,4 +8,6 @@ echo "Account ID: ${VENDED_ACCOUNT_ID}"
 ACCOUNT_NAME=$(aws dynamodb get-item --table-name aft-request-metadata --key "{\"id\": {\"S\": \"${VENDED_ACCOUNT_ID}\"}}" --profile aft-management | jq -r '.Item.account_name.S')
 echo "Account Name: ${ACCOUNT_NAME}"
 echo "Adding Account Name to Terraform Locals"
-sed -i "s|ACCOUNT_NAME|${ACCOUNT_NAME}|g" ${DEFAULT_PATH}/${CUSTOMIZATION}/locals.tf
+sed -i "s|ACCOUNT_NAME|${ACCOUNT_NAME}|g" ${DEFAULT_PATH}/${CUSTOMIZATION}/terraform/locals.tf
+echo "Checking Locals file"
+cat ${DEFAULT_PATH}/${CUSTOMIZATION}/terraform/locals.tf
