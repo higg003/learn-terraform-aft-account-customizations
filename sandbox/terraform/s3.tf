ed6c09de1ce46be721a1558d47787e4ea49c140d
Momcilo Dejanovic <momcilo.dejanovic.reyktech@sonymusic.com>
2023-07-28 13:09:17 +0200

    Testing providers

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -2,8 +2,6 @@ data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "sandbox_bucket" {
 
-  provider = aws.us
-
   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
 }
--- sandbox/terraform/ssm.tf
+++ sandbox/terraform/ssm.tf
@@ -1,7 +1,5 @@
 resource "aws_ssm_parameter" "foo" {
 
-  provider = aws.us
-
   name  = "/accID"
   type  = "String"
   value = data.aws_caller_identity.current.account_id
