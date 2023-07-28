acdf167c2e27ccd99383d97227b38c30f3bcb073
Momcilo Dejanovic <momcilo.dejanovic.reyktech@sonymusic.com>
2023-07-28 13:31:19 +0200

    Rollback

--- sandbox/terraform/vpc.tf
+++ sandbox/terraform/vpc.tf
@@ -1,26 +1,6 @@
-module "vpc_us" {
-
-  providers = {
-    aws = aws.us
-  }
+module "vpc" {
 
   source   = "../../modules/vpc"
   vpc_name = "${local.account_name}-us-vpc"
 
-}
-
-module "vpc_eu" {
-
-  providers = {
-    aws = aws.eu
-  }
-
-  source   = "../../modules/vpc"
-  vpc_name = "${local.account_name}-eu-vpc"
-
-}
-
-moved {
-  from = module.vpc
-  to   = module.vpc_us
 }
