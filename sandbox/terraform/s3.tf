f921fac4675270d921e9877d0c1490819cc838c2
Momcilo Dejanovic <momcilo.dejanovic.reyktech@sonymusic.com>
2023-07-28 11:21:46 +0200

    Remove excessive files

--- sandbox/terraform/s3.tf
+++
@@ -1,9 +0,0 @@
-data "aws_caller_identity" "current" {}
-
-resource "aws_s3_bucket" "sandbox_bucket" {
-  providers = {
-    aws = local.us_deploment ? aws.us_region : aws.eu_region
-  }
-  bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
-  acl    = "private"
-}
--- sandbox/terraform/ssm.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ssm_parameter" "foo" {
-  providers = {
-    aws = local.us_deploment ? aws.us_region : aws.eu_region
-  }
-  name  = "/accID"
-  type  = "String"
-  value = data.aws_caller_identity.current.account_id
-}
