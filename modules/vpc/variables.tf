53809f33c4e0455584ec8ee0f7eb693562ac9a66
Momcilo Dejanovic <momcilo.dejanovic.reyktech@sonymusic.com>
2023-07-31 18:01:40 +0200

    Fix validation issue

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -22,8 +22,8 @@ variable "az_list" {
   description = "List of AZs to use for the VPC"
   default     = ["us-east-1a", "us-east-1b"]
   validation {
-    condition     = contains(["us-east-1a", "us-east-1b", "eu-central-1a", "eu-central-1b"], var.az_list)
-    error_message = "Valid values for var: az_list are (us-east-1a, us-east-1b, eu-central-1a and eu-central-1b)."
+    condition     = length(var.az_list) > 0
+    error_message = "Variable var: az_list cannot be empty."
   }
 }
 
