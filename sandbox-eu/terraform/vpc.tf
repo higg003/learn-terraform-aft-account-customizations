dfdf8f08861a46d312a5394fee6379be7218496c
Momcilo Dejanovic <momcilo.dejanovic.reyktech@sonymusic.com>
2023-07-31 18:17:15 +0200

    Fix region data source

--- sandbox-eu/terraform/vpc.tf
+++ sandbox-eu/terraform/vpc.tf
@@ -1,7 +1,7 @@
 module "vpc" {
 
   source   = "../../modules/vpc"
-  aws_region = data.aws_caller_identity.current.name
+  aws_region = data.aws_region.current.name
   vpc_name = "${local.account_name}-eu-vpc"
   az_list  = ["eu-central-1a", "eu-central-1b"]
 }
